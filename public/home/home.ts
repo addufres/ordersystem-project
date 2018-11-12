34583b03efb895812d15a9bf09dcd9b7da5a0598
Dufresne <dufresa@essendant.com>
2018-11-12 17:33:46 -0600

    converting to typescript and adding webpack and webpack-dev-server

---
+++ public/home/home.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+var homeComponent = {
+    templateUrl : './home/home.html',
+    bindings: {},
+    controller: homeComponentController
+}
+
+function homeComponentController(){
+    var vm = this;
+    vm.title = 'Awesome, Inc. Internal Ordering System!!';
+}
+
+export default homeComponent;
