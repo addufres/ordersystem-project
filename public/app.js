618c6ccc2f19e0cff7c83a2527e99fce83d464d0
Dufresne <dufresa@essendant.com>
2018-11-12 17:35:10 -0600

    forgot some files

--- public/app.js
+++
@@ -1,8 +0,0 @@
-import homeComponent from './home/home';
-
-angular.module('app', ['ngRoute'])
-    .component('home', homeComponent);
-
-angular.module('app').config(['$locationProvider', function($locationProvider) {
-    $locationProvider.hashPrefix('');
-}]);
