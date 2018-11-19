6f34518b95b8dd5d06974eaf46796cdde5e30c7e
Dufresne <dufresa@essendant.com>
2018-11-19 10:57:16 -0600

    converted exported and imported routes and navigation to bundle and created a css file to eventually export and import into bundle as well

--- public/app.ts
+++ public/app.ts
@@ -2,14 +2,19 @@ import * as angular from 'angular';
 import 'angular-route';
 
 import hashPrefixConfig from './config.hashprefix';
+import routeProviderConfig from './config.routes';
 import homeComponent from './home/home';
+import navigationComponent from './navigation/navigation';
 import customersComponent from './customers/customers';
 import customerDetailComponent from './customerDetail/customerDetail';
 import CustomerService from './customers/customerService';
 import AddressService from './shared/addressService';
 
+
 angular.module('app', ['ngRoute'])
     .config(hashPrefixConfig)
+    .config(routeProviderConfig)
+    .component('navigation', navigationComponent)
     .component('home', homeComponent)
     .component('customers', customersComponent)
     .component('customerDetail', customerDetailComponent)
