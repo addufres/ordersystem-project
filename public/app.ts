ae37dabb4ceca8c275715f77cfd24e10d7a4fb10
Dufresne <dufresa@essendant.com>
2018-11-19 13:58:03 -0600

    validateDate and customers-table converted to modules and added to bundle.js

--- public/app.ts
+++ public/app.ts
@@ -9,14 +9,17 @@ import customersComponent from './customers/customers';
 import customerDetailComponent from './customerDetail/customerDetail';
 import CustomerService from './customers/customerService';
 import AddressService from './shared/addressService';
-
+import validateDateDirective from './directives/validateDate';
+import customersTableComponent from './customers/customers-table';
 
 angular.module('app', ['ngRoute'])
     .config(hashPrefixConfig)
     .config(routeProviderConfig)
+    .directive('validateDate', validateDateDirective)
     .component('navigation', navigationComponent)
     .component('home', homeComponent)
     .component('customers', customersComponent)
     .component('customerDetail', customerDetailComponent)
+    .component('customersTable', customersTableComponent)
     .service('customerService', CustomerService)
     .service('addressService', AddressService);
