9550c4b055291be7ec1a1c111ce86094d6fb206e
Dufresne <dufresa@essendant.com>
2018-11-20 14:37:26 -0600

    split webpack.config into environment config files in prep for production deploy

---
+++ public/webpack-configs/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports =  {
+    entry: './app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '..\\..\\', 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css?$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+                // styleLoader(cssLoader(styles))
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
